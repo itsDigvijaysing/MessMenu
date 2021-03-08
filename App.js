43fbd9e70c004b3634ea323ac79e3b8b0d902eda
Digvijaysing Rajput <geniusdigvijaysing@gmail.com>
2021-03-08 16:58:09 +0530

    Detail Screeen

--- App.js
+++ App.js
@@ -7,9 +7,11 @@ import {MaterialCommunityIcons} from '@expo/vector-icons';
 
 import AppText from './app/components/AppText';
 import AppButton from './app/components/AppButton';
+import Card from './app/components/Card';
+import DetailsScreen from './app/screens/DetailsScreen';
 
 export default function App() {
-  return <WelcomeScreen/>;
+  return <DetailsScreen />;
 }
 
 // const styles = StyleSheet.create({
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Text, Platform, StyleSheet } from 'react-native';
 
-function AppText({ children }) {
-    return <Text style={styles.text}>{children}</Text>
+function AppText({ children, style }) {
+    return <Text style={[styles.text, style]}>{children}</Text>
 }
 
 export default AppText;
